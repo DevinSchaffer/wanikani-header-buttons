7b752bc7a0e4fa4840e6573f7a32db7b5e4511b2
Devin Schaffer <dschaff6@gmail.com>
2021-12-31 20:43:19 -0500

    Initial commit.

---
+++ wanikani-header-buttons.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name			Always Show Top Lesson and Review buttons
+// @namespace		https://www.wanikani.com
+// @description		Always show the lesson and review buttons in the top bar without having to scroll below the giant buttons.
+// @author			Devin Schaffer
+// @version			1.0.0
+// @license			GNU-3.0
+// @include			/^https://(www|preview).wanikani.com/(dashboard)?$/
+// @grant			none
+// ==/UserScript==
+ 
+(() => {
+	const headerButtons = document.getElementsByClassName("navigation-shortcuts")[0];
+    headerButtons.classList.remove("hidden");
+ 
+	let observer = new MutationObserver((mutations, observer) => {
+    	mutations.forEach(mutation => {
+			if (mutation.attributeName == 'class') {
+				let classList = [...mutation.target.classList];
+ 
+				if (classList && classList.some(c => c == 'navigation-shortcuts') && classList.some(c => c == "hidden")) {
+					headerButtons.classList.remove("hidden");
+				}
+			}
+		});
+	});
+ 
+    observer.observe(document, { attributes: true, subtree:true });
+})();
